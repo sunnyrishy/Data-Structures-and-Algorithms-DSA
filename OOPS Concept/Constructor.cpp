bd19079ca07b98bd928886ee59774a2b98d2f04a
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2023-07-06 10:07:07 +0530

    constructor

---
+++ OOPS Concept/Constructor.cpp
@@ -0,0 +1,9 @@
+Constructor Properties : 
+
+1. No Return type
+2. Same Name as the Class name
+3. No Input Argument
+
+Work of the constructor is to initialize the object with garbage values and only called once at the creation time.
+
+  
