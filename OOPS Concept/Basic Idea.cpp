3d7578fb1250f9617b0f715cb738ad97af39e4cd
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2023-07-06 09:02:07 +0530

    basic_def

---
+++ OOPS Concept/Basic Idea.cpp
@@ -0,0 +1,6 @@
+Object Oriented Programming - It includes Class and Objects.
+
+Objects are the basic entities that have some properties and functions.
+
+A Class like a Template, if we need to use it we need to make a copy of it and use it for our object.
+1 Class can be used to create many objects.
