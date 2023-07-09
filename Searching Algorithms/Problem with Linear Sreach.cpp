767e802d7820af8fc53edf8fa5fbf55fbec71801
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2023-07-09 10:05:26 +0530

    problem_and_steps

---
+++ Searching Algorithms/Problem with Linear Sreach.cpp
@@ -0,0 +1,8 @@
+For the Sorted array it takes - n steps.
+
+For the unsorted array it takes - n steps.
+
+
+  Our need is to take only log(N) steps for the sorted search.
+
+  This can't be done using Linear Search, So Binary Search is used to search the sorted array in log(N) steps
