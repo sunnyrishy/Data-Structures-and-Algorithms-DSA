c7b1fd23614760466af82d0cddd60eef10d541df
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2023-08-15 10:52:40 +0530

    delete_at_a_index

--- Arrays/Delete at a Index.cpp
+++ Arrays/Delete at a Index.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 int main() {
-    vector<int> vec = {5, 10, 15, 20, 25};
+    vector<int> arr = {5, 10, 15, 20, 25};
     int indexToDelete = 2;
 
-    if (indexToDelete >= 0 && indexToDelete < vec.size()) {
-        vec.erase(vec.begin() + indexToDelete);
+    if (indexToDelete >= 0 && indexToDelete < arr.size()) {
+        arr.erase(arr.begin() + indexToDelete);
         cout << "Element at index " << indexToDelete << " deleted." << endl;
     } else {
         cout << "Invalid index." << endl;
     }
 
     cout << "Updated vector: ";
-    for (int i = 0; i < vec.size(); ++i) {
-        cout << vec[i] << " ";
+    for (int i = 0; i < arr.size(); ++i) {
+        cout << arr[i] << " ";
     }
     cout << endl;
 
