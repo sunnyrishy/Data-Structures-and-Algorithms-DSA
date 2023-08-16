9aff09870f275a13159b0ade1ab170cfa9477391
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2023-08-16 09:26:25 +0530

    bubble_sort

--- Sorting Algorithms/Bubble Sort.cpp
+++ Sorting Algorithms/Bubble Sort.cpp
@@ -33,3 +33,47 @@ int main() {
 
 Space Complexity :  O(1)
 Time Complexity : O(N^2) - Worst Case
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubblesort(vector<int> &arr, int S){
+    for(int i=0;i<S-1;i++){
+        for(int j=0;j<S-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {3,5,6,1,2,9,0};
+    int size = arr.size();
+    bubblesort(arr, size);
+    cout<<"Sorted array : ";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+
+
+Reference : https://www.geeksforgeeks.org/bubble-sort/
+
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements 
+if they are in the wrong order. 
+This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.
+
+In this algorithm, 
+
+traverse from left and compare adjacent elements and the higher one is placed at right side. 
+In this way, the largest element is moved to the rightmost end at first. 
+This process is then continued to find the second largest and place it and so on until the data is sorted.
+
+
+  Placing the larger numbers at the right most of the array is the logic of this sorting function.
