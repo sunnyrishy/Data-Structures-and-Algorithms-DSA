85bc8ae6c3a0d9b0b981bbf10ce9bc3ef69c94a2
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2024-03-02 11:52:55 +0530

    using adjacency list

--- Graphs/Graph Initialization Using List.cpp
+++ Graphs/Graph Initialization Using List.cpp
@@ -20,3 +20,35 @@ int main () {
         cout<<endl;
     }
 }
+
+
+
+
+
+
+Weighted Graph : 
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n = 5;
+    int m = 6;
+    vector<pair<int,int>> adj[n+1];   // we store the neighbout and its weights in a pair
+    for(int i = 0; i<m; i++){
+        int u,v, wt;
+        cin>> u >> v >>wt;
+        adj[u].push_back(make_pair(v, wt));
+        adj[v].push_back(make_pair(u, wt));
+    }
+    cout<<"Adjacency List "<<endl;
+    for(int i = 0; i<= n; i++){
+        cout<<"Vertex "<<i<<"  : ";
+        for(int j= 0; j<adj[i].size();j++){
+            cout<<" ("<<adj[i][j].first<<" , "<<adj[i][j].second<<" ) ";
+        }
+        cout<<endl;
+    }
+}
